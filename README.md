3b078750e02ff8dff7c203f4523e046a25f56cee
Nitish Devadiga <nitishdevadiga@hotmail.com>
2022-02-24 19:41:28 +0530

    Updated Deployed Rinkeby Addresses

--- README.md
+++ README.md
@@ -19,19 +19,19 @@ CX_NFT (Old Marketplace + ERC1155 Contract): `0x1dF12C7cE1c6E1598F2c9C663EcFaB0F
 ### **Current Deployed Contracts - (_Rinkeby_)**
 
 Proxy:
-[0x1e0186bA889F9e9a8F74466698Fcc95EFa260078](https://rinkeby.etherscan.io/address/0x1e0186bA889F9e9a8F74466698Fcc95EFa260078#code)
+[0x40E9aceebd6FD1Da3a60b3F7DCf19011446DC3d3](https://rinkeby.etherscan.io/address/0x40E9aceebd6FD1Da3a60b3F7DCf19011446DC3d3#code)
 
 Forwarder:
-[0x9f4411ED167290ad7D5a53141a67c3834dd0d3DE](https://rinkeby.etherscan.io/address/0x9f4411ED167290ad7D5a53141a67c3834dd0d3DE#code)
+[0x0Be6CA0eCBC45DEd5c2822BB8Ddc632b78575415](https://rinkeby.etherscan.io/address/0x0Be6CA0eCBC45DEd5c2822BB8Ddc632b78575415#code)
 
 Marketplace:
-[0x85CE81Fe52aa02Ee995Bf27DCC3435026a6A047E](https://rinkeby.etherscan.io/address/0x85CE81Fe52aa02Ee995Bf27DCC3435026a6A047E#code)
+[0xC2C19D03867c687Fd525E86Df376551218911Ca2](https://rinkeby.etherscan.io/address/0xC2C19D03867c687Fd525E86Df376551218911Ca2#code)
 
 Xpress Test Token:
-[0xB0Eae0B88D04f195f6c9e753f8dF9C91eb80d888](https://rinkeby.etherscan.io/address/0xB0Eae0B88D04f195f6c9e753f8dF9C91eb80d888#code)
+[0x14671973c4931C3BFf4A9C732cD8ba6447fe1253](https://rinkeby.etherscan.io/address/0x14671973c4931C3BFf4A9C732cD8ba6447fe1253#code)
 
 ERC1155 Factory:
-[0xa702ae280e969523f5a37d90be14179d3c17ba51](https://rinkeby.etherscan.io/address/0xa702ae280e969523f5a37d90be14179d3c17ba51#code)
+[0x7E9c6ef8677C0a96A39ec3c589C44e7C5A4a5b9E](https://rinkeby.etherscan.io/address/0x7E9c6ef8677C0a96A39ec3c589C44e7C5A4a5b9E#code)
 
 ERC721 Factory:
-[0x6b573572f66D4C23a68188964A86B0a2E17b09Da](https://rinkeby.etherscan.io/address/0x6b573572f66D4C23a68188964A86B0a2E17b09Da#code)
+[0xB8E09726e6a0eb757bd702B1fbE470856A83308f](https://rinkeby.etherscan.io/address/0xB8E09726e6a0eb757bd702B1fbE470856A83308f#code)
--- scripts/deployTest.js
+++ scripts/deployTest.js
@@ -10,64 +10,62 @@ async function main() {
    // ----- DEPLOYMENTS -----
 
    const CXRegistry = await ethers.getContractFactory('CX_Proxy_Registry');
-   const registry = await CXRegistry.deploy();
+   const registry = await CXRegistry.connect(deployer).deploy();
    console.log(`\nRegistry address -> ${registry.address}`);
 
    const FORWARDER = await ethers.getContractFactory('MinimalForwarder');
-   const forwarder = await FORWARDER.deploy();
+   const forwarder = await FORWARDER.connect(deployer).deploy();
    console.log(`\nForwarder address -> ${forwarder.address}`);
 
    const CX_MARKETPLACE = await ethers.getContractFactory('CX_Marketplace_V1');
-   const cx_marketplace = await CX_MARKETPLACE.deploy();
+   const cx_marketplace = await CX_MARKETPLACE.connect(deployer).deploy();
    console.log(`\nMarketplace address -> ${cx_marketplace.address}`);
 
    const XPRESS_TOKEN = await ethers.getContractFactory('Xpress');
-   const xpress = await XPRESS_TOKEN.deploy();
+   const xpress = await XPRESS_TOKEN.connect(deployer).deploy();
    console.log(`\nXpress address -> ${xpress.address}`);
 
    const CXFactoryERC1155 = await ethers.getContractFactory(
       'CX_Factory_ERC1155'
    );
-   const factoryERC1155 = await CXFactoryERC1155.deploy();
+   const factoryERC1155 = await CXFactoryERC1155.connect(deployer).deploy();
    console.log(`\nFactory (ERC1155) address -> ${factoryERC1155.address}`);
 
    const CXFactoryERC721 = await ethers.getContractFactory('CX_Factory_ERC721');
-   const factoryERC721 = await CXFactoryERC721.deploy();
+   const factoryERC721 = await CXFactoryERC721.connect(deployer).deploy();
    console.log(`\nFactory (ERC721) address -> ${factoryERC721.address}\n`);
 
    // ----- END OF DEPLOYMENTS -----
 
    // ----- INITIALIZATIONS -----
 
-   await xpress.initialize();
+   await xpress.connect(deployer).initialize();
    console.log('Xpress Test Token Initialized');
 
-   await cx_marketplace.initialize(
-      '1.0-Rinkeby',
-      registry.address,
-      forwarder.address
-   );
+   await cx_marketplace
+      .connect(deployer)
+      .initialize('1.0-Rinkeby', registry.address, forwarder.address);
    console.log('\nMarketplace Initialized');
 
-   await cx_marketplace.setPaymentTokenAllowed(xpress.address, true); // approve test xpress token
-   console.log('\nApproved Xpress as Valid Payment Token for Marketplace');
-
-   await registry.grantInitialAuthentication(cx_marketplace.address);
+   await registry
+      .connect(deployer)
+      .grantInitialAuthentication(cx_marketplace.address);
    console.log('\nMarketplace authenticated in Proxy Registry');
 
-   await factoryERC1155.initialize(
-      'CX Factory SFT',
-      registry.address,
-      forwarder.address
-   );
+   await factoryERC1155
+      .connect(deployer)
+      .initialize('CX Factory SFT', registry.address, forwarder.address);
    console.log('\nERC1155 Factory Initialized');
 
-   await factoryERC721.initialize(
-      'CX Factory NFT',
-      registry.address,
-      forwarder.address
-   );
+   await factoryERC721
+      .connect(deployer)
+      .initialize('CX Factory NFT', registry.address, forwarder.address);
    console.log('\nERC721 Factory Initialized');
+
+   await cx_marketplace
+      .connect(deployer)
+      .setPaymentTokenAllowed(xpress.address, true); // approve test xpress token
+   console.log('\nApproved Xpress as Valid Payment Token for Marketplace');
 
    // ----- END OF INITIALIZATIONS -----
 }
